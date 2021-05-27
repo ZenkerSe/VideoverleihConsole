2c5c4a74ec33907ec3f748787c940c545c74e1bf
ZenkerSe <Sebastian.Zenker@srh.de>
2021-05-27 15:42:47 +0200

    Projektdateien hinzufügen.

---
+++ VideoverleihConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VideoverleihConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

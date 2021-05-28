fcd281a638caab8dde552eb815b40c3b0ffb9aaa
ZenkerSe <31927581+ZenkerSe@users.noreply.github.com>
2021-05-28 14:29:14 +0200

    Models angelegt

---
+++ VideoverleihConsole/Model/Leihe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoverleihConsole.Model
+{
+    class Leihe
+    {
+        public Guid ID { get; }
+        public DateTime leihdatum { get; }
+        public int leihdauerInTagen { get; set; }
+
+        public string omdbID { get; }
+
+
+
+        public Leihe(DateTime Leihdatum, string OmdbID, int LeihdauerInTagen)
+        {
+            leihdatum = Leihdatum;
+            omdbID = OmdbID;
+            leihdauerInTagen = LeihdauerInTagen;
+
+        }
+
+        public Leihe(string OmdbID, int LeihdauerInTagen)
+        {
+            leihdatum = DateTime.Now;
+            omdbID = OmdbID;
+            leihdauerInTagen = LeihdauerInTagen;
+
+        }
+    }
+}

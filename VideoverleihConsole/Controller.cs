e87cec629b97780dff0da2b035e9970d48dd97e4
ZenkerSe <31927581+ZenkerSe@users.noreply.github.com>
2021-06-08 10:33:16 +0200

    ok

---
+++ VideoverleihConsole/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoverleihConsole.Model;
+
+namespace VideoverleihConsole
+{
+    public class Controller
+    {
+        public List<Kunde> Kunden { get; set; }
+      
+        public Controller(List<Kunde> kunden)
+        {
+            Kunden = kunden;
+        }
+            //Console.WriteLine("\n 4: Kunden anzeigen");
+            //Console.WriteLine("\n 5: Kunden anlegen");
+            //Console.WriteLine("\n 6: Kunden bearbeiten");
+            //Console.WriteLine("\n 7: Kunden löschen");
+            
+        public void KundenAnzeigen()
+        {
+            foreach(Kunde i in Kunden)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- VideoverleihConsole/Program.cs
+++ VideoverleihConsole/Program.cs
@@ -1,24 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using VideoverleihConsole.Model;
-using VideoverleihConsole.Controller;
+
 
 namespace VideoverleihConsole
 {
     class Program
     {
-        new KundenController Kundenliste = new KundenController();
-
-        
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello testung World!");
 
-            Kundenliste.kundeHinzufuegen("Müller","Lieschen", 30.04.89, "lieschenMueller@mueller.de");
+            List<Kunde> kunden = new List<Kunde>();
+            kunden.Add(new Kunde("Müller","Lieschen",Convert.ToDateTime("30.04.1989"),"liese@mueller.de"));
+            Controller Kundenliste = new Controller(kunden);
 
-           foreach(Kunde i in Kundenliste)
+            int decisionIdentifier = 0;
+            //Menue
+            while (decisionIdentifier != 8) { 
+
+            Console.WriteLine("Videoverleih - Consoletool");
+            Console.WriteLine("\n________________________");
+
+            Console.WriteLine("\n 0: Leihen anzeigen");
+
+            Console.WriteLine("\n 1: Leihe eintragen");
+            Console.WriteLine("\n 2: Leihe bearbeiten");
+            Console.WriteLine("\n 3: Leihe löschen");
+
+            Console.WriteLine("\n 4: Kunden anzeigen");
+
+
+            Console.WriteLine("\n 5: Kunden anlegen");
+            Console.WriteLine("\n 6: Kunden bearbeiten");
+            Console.WriteLine("\n 7: Kunden löschen");
+            Console.WriteLine("\n 8: Beenden");
+
+            decisionIdentifier = Convert.ToInt32(Console.ReadLine());
+
+            switch (decisionIdentifier)
             {
-
+                case 0:
+                    Console.WriteLine("Leihen anzeigen");
+                    break;
+                case 1:
+                    Console.WriteLine("Leihe eintragen");
+                    break;
+                case 2:
+                    Console.WriteLine("Leihe bearbeiten");
+                    break;
+                case 3:
+                    Console.WriteLine("Leihe löschen");
+                    break;
+                case 4:
+                    Console.WriteLine("Kunden anzeigen");
+                        Kundenliste.KundenAnzeigen();
+                    break;
+                case 5:
+                    Console.WriteLine("Kunden anlegen");
+                    break;
+                case 6:
+                    Console.WriteLine("Kunden bearbeiten");
+                    break;
+                case 7:
+                    Console.WriteLine("Kunden löschen");
+                    break;
+                case 8:
+                    Console.WriteLine("Beenden");
+                    Environment.Exit(0);
+                    break;
+                default: 
+                    Console.WriteLine("invalide Eingabe");
+                    break;
+            }
             }
 
         }
